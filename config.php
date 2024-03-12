8ee3193828dd14d62e147c0c6fcdba22ee80a370
Vineet60 <126579178+Vineet60@users.noreply.github.com>
2024-03-12 16:11:02 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+$conn = mysqli_connect('localhost','root','','shop_db',3307) or die('connection failed');
+
+// $dbhost = "localhost";
+// $dbuser = "root";
+// $dbpass = "";
+// $dbname = "shop_db";
+
+// if(!$conn = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname,3307))
+// {
+
+// 	die("failed to connect!");
+// }
+?>
